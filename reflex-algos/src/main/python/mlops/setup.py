b47a7f61f18ac9ca46bf1a03498f173e0b743afd
Zohar Mizrahi <zohar.mizrahi@parallelmachines.com>
2019-02-14 13:51:51 +0200

    [REF-5637] Fix and improve 'mlops' for PyPi publication ('ml-ops')

--- reflex-algos/src/main/python/mlops/setup.py
+++ reflex-algos/src/main/python/mlops/setup.py
@@ -1,27 +1,50 @@
+import os
 from setuptools import setup, find_packages
+
+from parallelm.mlops.constants import Constants
 
 import protobuf_dep
 protobuf_dep.copy_protobuf_sources()
 
 # This import should be placed after the copying of protobuf python sources
-from parallelm.mlops import version, project_name
 
-name = "parallelm"
-release = version
-desc = "mlops python library to interfance with the MLOps"
-lic = "ParallelM"
+# The directory containing this file
+ROOT = os.path.dirname(os.path.abspath(__file__))
 
-required_python_pkgs = ["pandas", "kazoo", "termcolor"]
+# The text of the README file
+README = open(ROOT + "/README.md").read()
 
 setup(
-    name=name,
+    name=Constants.OFFICIAL_NAME,
     namespace_packages=['parallelm'],
-    version=release,
-    description=desc,
-    license=lic,
+    version=Constants.MLOPS_CURRENT_VERSION,
+    description="A library to read and report MLApp statistics",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mlpiper/mlpiper/tree/master/reflex-algos/src/main/python/mlops",
+    author="ParallelM",
+    author_email="info@parallelm.com",
+    license="Apache License 2.0",
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+    ],
     zip_safe=False,
-    scripts=[],
-    install_requires=required_python_pkgs,
+    include_package_data=True,
+    package_data={'': ['*.json', '*.jar', '*.egg']},
     packages=find_packages(exclude=["build", "*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        "pandas",
+        "termcolor",
+        "kazoo",
+        "protobuf",
+        "requests"
+    ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    entry_points={
+    }
 )
-
