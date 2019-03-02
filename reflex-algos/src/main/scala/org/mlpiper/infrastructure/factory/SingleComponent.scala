ebcd97a489497068dc4b7e71b43a056bc189c706
Drew Roselli <drew.roselli@parallelm.com>
2019-03-01 16:48:56 -0800

    Move TensorflowComponent functionality into SingleComponent

--- reflex-algos/src/main/scala/org/mlpiper/infrastructure/factory/SingleComponent.scala
+++ reflex-algos/src/main/scala/org/mlpiper/infrastructure/factory/SingleComponent.scala
@@ -142,6 +142,107 @@ abstract class SingleComponent(val componentMetadata: ComponentMetadata,
   def componentDir(): String = Paths.get(componentsDir, componentMetadata.name).toString
 
   /**
+    * If the user provides a value, we do not override it.
+    * To do this, the json must provide a parameter that
+    * is tagged as the modelDirArgument.
+    * If the tagged argument is provided but left blank,
+    * then we override it with the system
+    * parameter modelOutputPath.
+    * This function is called from java, so it cannot return
+    * Options.
+    * @return String representing model save directory or empty string
+    */
+  def getModelSaveDirectory(): String = {
+    if (logDirArgument.isDefined) {
+      val modelDirArg = modelDirArgument.get
+      val modelDir = paramMap.get(modelDirArg)
+
+      modelDir match {
+        case Some(s) => {
+          val dir = s.asInstanceOf[String]
+          if (dir.length() > 0) {
+            return s.asInstanceOf[String]
+          }
+        }
+        case None => {}
+      }
+    }
+
+    if (modelOutputPath.isDefined) {
+      logger.debug("getModelSaveDirectory: using modelOutputPath")
+      return modelOutputPath.get
+    }
+
+    logger.debug("getModelSaveDirectory: found nothing")
+
+    return ""
+  }
+
+  /**
+    * To set this, the user must provide a value in a parameter
+    * tagged as the modelVersionArgument. Otherwise, it is left blank.
+    * This function is called from java, so it cannot return
+    * Options.
+    * @return String representing model version or empty string
+    */
+  def getModelVersion(): String = {
+    if (modelVersionArgument.isDefined) {
+      val modelVersionArg = modelVersionArgument.get
+      val modelVersion = paramMap.get(modelVersionArg)
+
+      modelVersion match {
+        case Some(s) => {
+          val vers = s.asInstanceOf[String]
+          if (vers.length() > 0) {
+            return vers
+          }
+        }
+        case None => {}
+      }
+    }
+    logger.debug("getModelVersion: modelVersion return empty string")
+
+    return ""
+  }
+
+  /**
+    * If the user provides a value, we do not override it.
+    * To do this, the json must provide a parameter that
+    * is tagged as the logDirArgument.
+    * If the tagged argument is provided but left blank,
+    * then we override it with the system
+    * parameter modelLogPath.
+    * This function is called from java, so it cannot return
+    * Options.
+    * @return String representing log directory or empty string
+    */
+  def getLogSaveDirectory(): String = {
+    if (logDirArgument.isDefined) {
+      val logDirArg = logDirArgument.get
+      val logDir = paramMap.get(logDirArg)
+
+      logDir match {
+        case Some(s) => {
+          val dir = s.asInstanceOf[String]
+          if (dir.length() > 0) {
+            return s.asInstanceOf[String]
+          }
+        }
+        case None => {}
+      }
+    }
+
+    if (modelLogPath.isDefined) {
+      logger.debug("getLogSaveDirectory: using modelLogPath")
+      return modelLogPath.get
+    }
+
+    logger.debug("getLogSaveDirectory: found nothing")
+
+    return ""
+  }
+
+  /**
     * Building command line from the provided parameters during configure and those provided in the componentMetadata
     *
     * @param overrideModelDir If true then override the model path (input or output) even if it exists in the cmd line
