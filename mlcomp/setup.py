f56e67a4c250a0b044d5722945e38b33e9733446
Zohar Mizrahi <zohar.mizrahi@parallelmachines.com>
2019-03-14 12:27:44 +0200

    [REF-5897] Add missing dependent packages to mlcomp setup

--- mlcomp/setup.py
+++ mlcomp/setup.py
@@ -10,7 +10,7 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 # The text of the README file
 README = open(ROOT + "/README.md").read()
 
-install_requires = ['ml-ops', 'termcolor', 'flask', 'psutil', 'py4j']
+install_requires = ['ml-ops', 'termcolor', 'flask', 'flask_cors', 'psutil', 'py4j', 'nbformat', 'nbconvert']
 if sys.version_info[0] < 3:
     install_requires.append('enum')
 
