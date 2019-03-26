01ee7b4a5103a49b47948a192b57b5271dd34812
Zohar Mizrahi <zohar.mizrahi@parallelmachines.com>
2019-03-26 15:25:22 +0200

    [REF-5975] Fix packaging namespace packages (mlops, mlcomp)

--- mlops/parallelm/__init__.py
+++ mlops/parallelm/__init__.py
@@ -1,4 +1,1 @@
-__import__('pkg_resources').declare_namespace(__name__)
-
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
