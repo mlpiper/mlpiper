4d977cfcba9f340bb37bc03ea60060386af68379
Lior Amar <lior.amar@parallelmachines.com>
2019-03-29 15:17:26 -0700

    [REF-6013] mcenter-cli: few fixes
    o Remove trailing / at the end of a directory path in component-upload

--- mcenter_cli/parallelm/mcenter_cli/upload_component.py
+++ mcenter_cli/parallelm/mcenter_cli/upload_component.py
@@ -11,6 +11,9 @@ def upload_component(mcenter_client, comp_dir):
     :param comp_dir:
     """
 
+    if not os.path.isdir(comp_dir):
+        raise Exception("{} : is not a directory".format(comp_dir))
+    comp_dir = comp_dir.strip(os.sep)
     component_tar_path = os.path.join("/tmp", "component.{}.tar".format(uuid.uuid4()))
     with tarfile.open(component_tar_path, "w:") as tar:
         tar.add(comp_dir, arcname=os.path.basename(comp_dir))
