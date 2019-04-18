629d7e5fe0db5fe43db46829953939ee00e9bd76
harshil <harshil2493@gmail.com>
2019-04-17 20:37:40 -0700

    [REF-6034] MLOpsMetrics: Beautify MLOps Metrics

--- mlops/parallelm/mlops/mlops_metrics.py
+++ mlops/parallelm/mlops/mlops_metrics.py
@@ -37,6 +37,15 @@ class MLOpsMetrics(object):
 
     >>> mlops.metrics.explained_variance_score(y_true=labels_actual, y_pred=labels_pred)
 
+    >>> mlops.metrics.mean_absolute_error(y_true=labels, y_pred=labels_pred)
+
+    >>> mlops.metrics.mean_squared_error(y_true=labels, y_pred=labels_pred)
+
+    >>> mlops.metrics.mean_squared_log_error(y_true=labels, y_pred=labels_pred)
+
+    >>> mlops.metrics.median_absolute_error(y_true=labels, y_pred=labels_pred)
+
+    >>> mlops.metrics.r2_score(y_true=labels, y_pred=labels_pred)
     """
 
     ##################################################################
@@ -44,6 +53,16 @@ class MLOpsMetrics(object):
     ##################################################################
     @staticmethod
     def accuracy_score(y_true, y_pred, normalize=True, sample_weight=None):
+        """
+        Method calculates accuracy and output it using MCenter.
+
+        :param y_true: Ground truth (correct) labels.
+        :param y_pred: Predicted labels, as returned by a classifier.
+        :param normalize: If False, return the number of correctly classified samples. Otherwise, return the fraction of correctly classified samples.
+        :param sample_weight: weight of samples
+        :return: accuracy_score
+        """
+
         # need to import only on run time.
         from parallelm.mlops import mlops as mlops
         import sklearn
@@ -53,8 +72,18 @@ class MLOpsMetrics(object):
 
         mlops.set_stat(ClassificationMetrics.ACCURACY_SCORE, data=accuracy_score)
 
+        return accuracy_score
+
     @staticmethod
     def auc(x, y, reorder="deprecated"):
+        """
+        Method calculates auc and output it using MCenter.
+
+        :param x: x coordinates. These must be either monotonic increasing or monotonic decreasing.
+        :param y: y coordinates.
+        :param reorder: Whether to sort x before computing.
+        :return: auc score
+        """
         # need to import only on run time.
         from parallelm.mlops import mlops as mlops
         import sklearn
@@ -63,8 +92,19 @@ class MLOpsMetrics(object):
 
         mlops.set_stat(ClassificationMetrics.AUC, data=auc)
 
+        return auc
+
     @staticmethod
     def average_precision_score(y_true, y_score, average="macro", sample_weight=None):
+        """
+        Method calculates average precision value and output it using MCenter.
+
+        :param y_true: True binary labels or binary label indicators.
+        :param y_score: Target scores, can either be probability estimates of the positive class, confidence values, or non-thresholded measure of decisions (as returned by "decision_function" on some classifiers).
+        :param average: If None, the scores for each class are returned. It can be "micro", "macro", "weighted" or "samples"
+        :param sample_weight: Sample weights.
+        :return: average precision score
+        """
         # need to import only on run time.
         from parallelm.mlops import mlops as mlops
         import sklearn
@@ -76,8 +116,19 @@ class MLOpsMetrics(object):
 
         mlops.set_stat(ClassificationMetrics.AVERAGE_PRECISION_SCORE, data=aps)
 
+        return aps
+
     @staticmethod
     def confusion_matrix(y_true, y_pred, labels, sample_weight=None):
+        """
+        Method calculates confusion matrix and output it using MCenter as table.
+
+        :param y_true: Ground truth (correct) target values.
+        :param y_pred: Estimated targets as returned by a classifier.
+        :param labels: List of labels to index the matrix.
+        :param sample_weight: Sample weights.
+        :return: confusion matrix
+        """
         # need to import only on run time.
         from parallelm.mlops import mlops as mlops
         import sklearn
@@ -86,12 +137,23 @@ class MLOpsMetrics(object):
 
         mlops.set_stat(ClassificationMetrics.CONFUSION_MATRIX, data=cm, labels=labels)
 
+        return cm
+
     ##################################################################
     ######################## regression stats ########################
     ##################################################################
 
     @staticmethod
     def explained_variance_score(y_true, y_pred, sample_weight=None, multioutput="uniform_average"):
+        """
+        Method calculates explained variance score and output it using MCenter.
+
+        :param y_true: Ground truth (correct) target values.
+        :param y_pred: Estimated target values.
+        :param sample_weight: Sample weights.
+        :param multioutput: Defines aggregating of multiple output scores. It can be "raw_values", "uniform_average", "variance_weighted"
+        :return: explained variance score
+        """
         # need to import only on run time.
         from parallelm.mlops import mlops as mlops
         import sklearn
@@ -103,8 +165,19 @@ class MLOpsMetrics(object):
 
         mlops.set_stat(RegressionMetrics.EXPLAINED_VARIANCE_SCORE, data=evs)
 
+        return evs
+
     @staticmethod
     def mean_absolute_error(y_true, y_pred, sample_weight=None, multioutput="uniform_average"):
+        """
+        Method calculates mean absolute error and output it using MCenter.
+
+        :param y_true: Ground truth (correct) target values.
+        :param y_pred: Estimated target values.
+        :param sample_weight: Sample weights.
+        :param multioutput: Defines aggregating of multiple output scores. It can be "raw_values", "uniform_average"
+        :return: mean absolute error
+        """
         # need to import only on run time.
         from parallelm.mlops import mlops as mlops
         import sklearn
@@ -116,8 +189,19 @@ class MLOpsMetrics(object):
 
         mlops.set_stat(RegressionMetrics.MEAN_ABSOLUTE_ERROR, data=mae)
 
+        return mae
+
     @staticmethod
     def mean_squared_error(y_true, y_pred, sample_weight=None, multioutput="uniform_average"):
+        """
+        Method calculates mean squared error and output it using MCenter.
+
+        :param y_true: Ground truth (correct) target values.
+        :param y_pred: Estimated target values.
+        :param sample_weight: Sample weights.
+        :param multioutput: Defines aggregating of multiple output scores. It can be "raw_values", "uniform_average"
+        :return: mean squared error
+        """
         # need to import only on run time.
         from parallelm.mlops import mlops as mlops
         import sklearn
@@ -129,8 +213,19 @@ class MLOpsMetrics(object):
 
         mlops.set_stat(RegressionMetrics.MEAN_SQUARED_ERROR, data=mse)
 
+        return mse
+
     @staticmethod
     def mean_squared_log_error(y_true, y_pred, sample_weight=None, multioutput="uniform_average"):
+        """
+        Method calculates mean squared log error and output it using MCenter.
+
+        :param y_true: Ground truth (correct) target values.
+        :param y_pred: Estimated target values.
+        :param sample_weight: Sample weights.
+        :param multioutput: Defines aggregating of multiple output scores. It can be "raw_values", "uniform_average"
+        :return: mean squared log error
+        """
         # need to import only on run time.
         from parallelm.mlops import mlops as mlops
         import sklearn
@@ -142,8 +237,18 @@ class MLOpsMetrics(object):
 
         mlops.set_stat(RegressionMetrics.MEAN_SQUARED_LOG_ERROR, data=msle)
 
+        return msle
+
     @staticmethod
     def median_absolute_error(y_true, y_pred):
+        """
+        Method calculates median absolute error and output it using MCenter.
+
+        :param y_true: Ground truth (correct) target values.
+        :param y_pred: Estimated target values.
+
+        :return: median absolute error
+        """
         # need to import only on run time.
         from parallelm.mlops import mlops as mlops
         import sklearn
@@ -153,8 +258,20 @@ class MLOpsMetrics(object):
 
         mlops.set_stat(RegressionMetrics.MEDIAN_ABSOLUTE_ERROR, data=mae)
 
+        return mae
+
     @staticmethod
     def r2_score(y_true, y_pred, sample_weight=None, multioutput="uniform_average"):
+        """
+        Method calculates r2 score and output it using MCenter.
+
+        :param y_true: Ground truth (correct) target values.
+        :param y_pred: Estimated target values.
+        :param sample_weight: Sample weights.
+        :param multioutput: Defines aggregating of multiple output scores. It can be "raw_values", "uniform_average", "variance_weighted"
+        :return: r2 score
+        """
+
         # need to import only on run time.
         from parallelm.mlops import mlops as mlops
         import sklearn
@@ -165,3 +282,5 @@ class MLOpsMetrics(object):
                                             multioutput=multioutput)
 
         mlops.set_stat(RegressionMetrics.R2_SCORE, data=r2_score)
+
+        return r2_score
