173bfd1df22c2773ea5f4c5c8da62ebc5d763cf0
harshil <harshil2493@gmail.com>
2019-04-18 19:23:08 -0700

    [REF-6034] MLOPs Metrics Beautification 1: Adding Classification/Regression StatObjectFactory
    - Classes are responsible for creating stat object from given input.
    - Removing big if elses and taking advantage of dicts.
    
    [REF-6034] MLOPs Metrics Beautification 1: Adding Single Value & Table Value Object Creation From `single_value` and `2d_list` Respectively
    
    Testing Done:
    - Unit Tests

--- mlops/parallelm/mlops/metrics_constants.py
+++ mlops/parallelm/mlops/metrics_constants.py
@@ -4,6 +4,7 @@ from enum import Enum
 class ClassificationMetrics(Enum):
     """
     Class will hold predefined naming of all classification ML Metrics supported by ParallelM.
+    WWhen adding a new enum, register a stat creation function to ClassificationStatObjectFactory.registry_name_to_function.
     """
     ACCURACY_SCORE = "Accuracy Score"
     AUC = "AUC(Area Under the Curve)"
@@ -17,6 +18,7 @@ class ClassificationMetrics(Enum):
 class RegressionMetrics(Enum):
     """
     Class will hold predefined naming of all regression ML Metrics supported by ParallelM.
+    When adding a new enum, register a stat creation function to RegressionStatObjectFactory.registry_name_to_function.
     """
     EXPLAINED_VARIANCE_SCORE = "Explained Variance Score"
     MEAN_ABSOLUTE_ERROR = "Mean Absolute Error"
