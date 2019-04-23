c899457b3a264ef8af8d0d45d00dd2255a21e091
Yakov Goldberg <yakov.goldberg@parallelmachines.com>
2019-04-23 11:38:17 -0700

    Point mlops/mlcomp homepages to GitHub Pages
    Github pages is github powered web page, generated from README.md or
    docs.

--- mlcomp/setup.py
+++ mlcomp/setup.py
@@ -20,7 +20,7 @@ setup(
     description="An engine for running component based ML pipelines",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/mlpiper/mlpiper/tree/master/reflex-algos/src/main/python/mlcomp",
+    url="https://mlpiper.github.io/mlpiper/mlcomp/",
     author="ParallelM",
     author_email="info@parallelm.com",
     license="Apache License 2.0",
--- mlops/setup.py
+++ mlops/setup.py
@@ -22,7 +22,7 @@ setup(
     description="A library to read and report MLApp statistics",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/mlpiper/mlpiper/tree/master/reflex-algos/src/main/python/mlops",
+    url="https://mlpiper.github.io/mlpiper/mlops/",
     author="ParallelM",
     author_email="info@parallelm.com",
     license="Apache License 2.0",
