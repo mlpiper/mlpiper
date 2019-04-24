15d91b7e6ab6e462ea3b5d4588e09b4be774af9d
harshil <harshil2493@gmail.com>
2019-04-23 17:19:54 -0700

    [REF-6034] MAE: Adding Support For Multiple Float Return Value From Mean Absolute Error
    Usage:
        labels_2d_actual = [[1.0, 0.5], [2.5, 4.75], [7.0, 0.75]]
        labels_2d_pred = [[1.5, 0.75], [2.75, 4.5], [7.50, 0.25]]
    
        mae = pm.metrics.mean_absolute_error(y_true=labels_2d_actual,
                                             y_pred=labels_2d_pred,
                                             multioutput="raw_values")

--- mlops/tests/test_regression_metrics.py
+++ mlops/tests/test_regression_metrics.py
@@ -18,6 +18,7 @@ def test_mlops_explained_variance_score_apis():
     # first way
     pm.set_stat(RegressionMetrics.EXPLAINED_VARIANCE_SCORE, evs)
 
+    # array list is allowed as well
     pm.set_stat(RegressionMetrics.EXPLAINED_VARIANCE_SCORE, [1, 2, 3])
 
     # second way
@@ -38,10 +39,13 @@ def test_mlops_explained_variance_score_apis():
     labels_2d_actual = [[1.0, 0.5], [2.5, 4.75], [7.0, 0.75]]
     labels_2d_pred = [[1.5, 0.75], [2.75, 4.5], [7.50, 0.25]]
 
-    # testing with sample weights as well
-    pm.metrics.explained_variance_score(y_true=labels_2d_actual,
-                                        y_pred=labels_2d_pred,
-                                        multioutput="raw_values")
+    # testing where result will be multiple float values
+    evs = pm.metrics.explained_variance_score(y_true=labels_2d_actual,
+                                              y_pred=labels_2d_pred,
+                                              multioutput="raw_values")
+
+    assert len(evs) == 2
+
     pm.done()
 
 
@@ -56,12 +60,11 @@ def test_mlops_mean_absolute_error_apis():
     # first way
     pm.set_stat(RegressionMetrics.MEAN_ABSOLUTE_ERROR, mae)
 
+    # array list is allowed as well
+    pm.set_stat(RegressionMetrics.MEAN_ABSOLUTE_ERROR, [1, 2, 3])
+
     # second way
     pm.metrics.mean_absolute_error(y_true=labels_actual, y_pred=labels_pred)
-
-    # should throw error if not numeric number is provided
-    with pytest.raises(MLOpsStatisticsException):
-        pm.set_stat(RegressionMetrics.MEAN_ABSOLUTE_ERROR, [1, 2, 3])
 
     # should throw error if labels predicted is different length than actuals
     with pytest.raises(ValueError):
@@ -74,6 +77,15 @@ def test_mlops_mean_absolute_error_apis():
     pm.metrics.mean_absolute_error(y_true=labels_actual,
                                    y_pred=labels_pred,
                                    sample_weight=sample_weight)
+
+    labels_2d_actual = [[1.0, 0.5], [2.5, 4.75], [7.0, 0.75]]
+    labels_2d_pred = [[1.5, 0.75], [2.75, 4.5], [7.50, 0.25]]
+
+    mae = pm.metrics.mean_absolute_error(y_true=labels_2d_actual,
+                                         y_pred=labels_2d_pred,
+                                         multioutput="raw_values")
+
+    assert len(mae) == 2
 
     pm.done()
 
