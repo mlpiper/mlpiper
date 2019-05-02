211100d9bc53b3dabca143bd69f766254bd4bad0
harshil <harshil2493@gmail.com>
2019-05-01 17:10:35 -0700

    [REF-6034] ContingencyMatrix : Adding MLOps Clustering Metrics - Contingency Matrix
    - Adding numpy requirement in setup.py
    
    Usages of APIs:
        labels_pred = [1, 0, 1, 2, 3, 0]
        labels_actual = [0, 1, 0, 1, 3, 1]
    
        # list of sorted labels. i.e. [0, 1, 2, ..]
        pred_labels = sorted(set(labels_pred))
        true_labels = sorted(set(labels_actual))
    
        cm = metrics.cluster.contingency_matrix(labels_actual, labels_pred, eps=None)
    
        # first way
        mlops.set_stat(ClusteringMetrics.CONTINGENCY_MATRIX,
                       data=cm,
                       true_labels=true_labels,
                       pred_labels=pred_labels)
    
        # second way
        mlops.metrics.cluster.contingency_matrix(labels_actual, labels_pred)
    
    Testing Done:
        - Unit Test

--- mlops/setup.py
+++ mlops/setup.py
@@ -45,7 +45,8 @@ setup(
         "protobuf",
         "requests",
         "py4j",
-        "scikit-learn"
+        "scikit-learn",
+        "numpy"
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     entry_points={
--- mlops/tests/test_clustering_metrics.py
+++ mlops/tests/test_clustering_metrics.py
@@ -109,3 +109,44 @@ def test_mlops_completeness_score_apis():
         mlops.metrics.completeness_score(labels_true=labels_actual, labels_pred=labels_pred_missing_values)
 
     mlops.done()
+
+
+def test_mlops_contingency_matrix_apis():
+    mlops.init(ctx=None, mlops_mode=MLOpsMode.STAND_ALONE)
+
+    labels_pred = [1, 0, 1, 2, 3, 0]
+    labels_actual = [0, 1, 0, 1, 3, 1]
+
+    # list of sorted labels. i.e. [0, 1, 2, ..]
+    pred_labels = sorted(set(labels_pred))
+    true_labels = sorted(set(labels_actual))
+
+    cm = metrics.cluster.contingency_matrix(labels_actual, labels_pred, eps=None)
+
+    # first way
+    mlops.set_stat(ClusteringMetrics.CONTINGENCY_MATRIX,
+                   data=cm,
+                   true_labels=true_labels,
+                   pred_labels=pred_labels)
+
+    # should throw error if true and pred labels list is missing
+    with pytest.raises(MLOpsStatisticsException):
+        mlops.set_stat(ClusteringMetrics.CONTINGENCY_MATRIX,
+                       data=cm)
+
+    # should throw error if true labels list is missing
+    with pytest.raises(MLOpsStatisticsException):
+        mlops.set_stat(ClusteringMetrics.CONTINGENCY_MATRIX,
+                       data=cm,
+                       pred_labels=pred_labels)
+
+    # should throw error if pred labels list is missing
+    with pytest.raises(MLOpsStatisticsException):
+        mlops.set_stat(ClusteringMetrics.CONTINGENCY_MATRIX,
+                       data=cm,
+                       true_labels=true_labels)
+
+    # second way
+    mlops.metrics.cluster.contingency_matrix(labels_actual, labels_pred)
+
+    mlops.done()
