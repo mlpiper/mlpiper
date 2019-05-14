41b0a28ab4cf3ced6261e1d864d7041d8b8f399d
Zohar Mizrahi <zohar.mizrahi@parallelmachines.com>
2019-05-14 12:21:57 +0300

    [REF-5994] Metrics are reported only upon change in value

--- mlcomp/tests/components/sagemaker/common/report.py
+++ mlcomp/tests/components/sagemaker/common/report.py
@@ -5,13 +5,20 @@ from parallelm.mlops.stats.table import Table
 
 
 class Report(object):
+    _last_metric_values = {}
 
     @staticmethod
     def job_status(job_name, status):
-        tbl = Table().name("SageMaker Job Status").cols(["Job Name", "Update Time", "Status"])
-        tbl.add_row([job_name, strftime("%Y/%m/%d %H:%M:%S", gmtime()), status])
-        mlops.set_stat(tbl)
+        last_value = Report._last_metric_values.get(job_name)
+        if last_value is None or last_value != status:
+            Report._last_metric_values[job_name] = status
+            tbl = Table().name("SageMaker Job Status").cols(["Job Name", "Update Time", "Status"])
+            tbl.add_row([job_name, strftime("%Y/%m/%d %H:%M:%S", gmtime()), status])
+            mlops.set_stat(tbl)
 
     @staticmethod
     def job_metric(metric_name, value):
-        mlops.set_stat(metric_name, value)
+        last_value = Report._last_metric_values.get(metric_name)
+        if last_value is None or last_value != value:
+            Report._last_metric_values[metric_name] = value
+            mlops.set_stat(metric_name, value)
