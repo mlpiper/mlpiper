eb1e2c166002cc4c71a70b68e4b4cd8c62f81947
Yakov Goldberg <yakov.goldberg@parallelmachines.com>
2019-06-05 22:55:26 -0700

    [REF-6233] add 'future' prerequisite for mlops

--- mlops/setup.py
+++ mlops/setup.py
@@ -46,7 +46,8 @@ setup(
         "requests",
         "py4j",
         "scikit-learn",
-        "numpy"
+        "numpy",
+        "future"
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     entry_points={
